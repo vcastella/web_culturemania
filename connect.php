e463289722dfdf881864922319cf46a04e884913
vcastella <vcastella@yacast.fr>
2017-08-31 10:55:03 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 	// Connexion et sélection de la base DEV
-	$mysql = new MySQLi('localhost', 'root', '', 'culturemdmania');
+	$mysql = new MySQLi('localhost', 'root', '', 'tete');
 							
-	//$linkDB = mysql_connect('mysql51-108.perso', 'culturemdmania', 'tjEr4MYUAaZb') or die('Impossible de se connecter : ' . mysql_error());
-	//mysql_select_db('culturemdmania') or die('Impossible de sélectionner la base de données');
-	//$mysql = new MySQLi('mysql51-108.perso', 'culturemdmania', 'tjEr4MYUAaZb', 'culturemdmania');
+	//$linkDB = mysql_connect('etnontulauraspas', 'degage', 'celuilanonplus') or die('Impossible de se connecter : ' . mysql_error());
+	//mysql_select_db('tete') or die('Impossible de sélectionner la base de données');
+	//$mysql = new MySQLi('etnontulauraspas', 'degage', 'celuilanonplus', 'tiretoi');
 	
 	$mysql->set_charset("utf8");			
 	
